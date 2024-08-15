9891b968d6ee288893239100718fe2f231929fa3
Wanashu <facundo.valeriano@gmail.com>
2024-08-14 23:35:34 -0300

    creation of components - basic flex and - routes not working

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { ToBuyComponent } from './main/to-buy/to-buy.component';
+import { GuideComponent } from './main/guide/guide.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+        {
+          path: 'tobuy',
+          component: ToBuyComponent,
+        },
+        {
+          path: 'guide', // example.com/about
+          component: GuideComponent,
+        },
+        // {
+        //   path: 'manage',
+        //   component: ManageComponent,
+        //   data: {
+        //     authOnly: true,
+        //     authGuardPipe: redirectUnauthorizedToHome,
+        //   },
+        //   canActivate: [AuthGuard],
+        // },
+        // {
+        //   path: '**',
+        //   component: NotFoundComponent,
+        // },
+      ];
---
+++ src/app/main/to-buy/to-buy.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-to-buy',
+  standalone: true,
+  imports: [],
+  templateUrl: './to-buy.component.html',
+  styleUrl: './to-buy.component.css'
+})
+export class ToBuyComponent {
+
+}
